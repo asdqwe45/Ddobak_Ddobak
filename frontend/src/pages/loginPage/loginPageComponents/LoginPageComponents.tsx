59a65cbb025c56044a5804ae3b3855e417d5d04d
LEETAESEONG <ga1754@naver.com>
2023-10-23 23:35:03 +0900

    <feat>[#090-LTS] 회원가입 페이지 구조 개발

---
+++ frontend/src/pages/loginPage/loginPageComponents/LoginPageComponents.tsx
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+export const Forgot = styled.div`
+  width: 300px;
+  display: flex;
+  justify-content: flex-end;
+  font-size: 12px;
+  font-weight: 500;
+`;
