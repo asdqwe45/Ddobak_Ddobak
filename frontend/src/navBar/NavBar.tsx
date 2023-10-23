a72d147ad5615bbf93c9f97a01454b1147e221d8
LEETAESEONG <ga1754@naver.com>
2023-10-23 14:10:04 +0900

    <feat>[#085-LTS] 회원가입 페이지 작성 전 구조 설계

--- frontend/src/navBar/NavBar.tsx
+++ frontend/src/navBar/NavBar.tsx
@@ -1,41 +1,53 @@
-import { NavLink } from 'react-router-dom'
-import classes from './NavBar.module.css'
-import NavLogo from './navBarAssets/navLogo.png'
+import { NavLink } from 'react-router-dom';
+import classes from './NavBar.module.css';
+import NavLogo from './navBarAssets/ddobak_logo.png';
 
 const NavBar: React.FC = () => {
   return (
     <>
       <nav className={classes.header}>
         <div className={classes.list}>
-          <NavLink to="/" className={({ isActive }) => (isActive ? classes.active : undefined)}>
-            <img src={NavLogo} alt="NavLogo" className={classes.navImage} />
-          </NavLink>
-
-          <NavLink
-            to="/fontMake"
-            className={({ isActive }) => (isActive ? classes.active : classes.navPont)}
-          >
-            제작하기
-          </NavLink>
-          <NavLink
-            to="/fontList"
-            className={({ isActive }) => (isActive ? classes.active : undefined)}
-          >
-            PontList
-          </NavLink>
-          <NavLink
-            to="/faqPage"
-            className={({ isActive }) => (isActive ? classes.active : undefined)}
-          >
-            Question
-          </NavLink>
-          <NavLink
-            to="/login"
-            className={({ isActive }) => (isActive ? classes.active : undefined)}
-          >
-            로그인 해주세요
-          </NavLink>
-          {/* <NavLink
+          <div className={classes.leftBox}>
+            <div className={classes.smallBox}>
+              <NavLink to="/">
+                <img src={NavLogo} alt="NavLogo" className={classes.navImage} />
+              </NavLink>
+            </div>
+            <div className={classes.smallBox}>
+              <NavLink
+                to="/fontMake"
+                className={({ isActive }) => (isActive ? classes.active : undefined)}
+              >
+                제작하기
+              </NavLink>
+            </div>
+            <div className={classes.smallBox}>
+              <NavLink
+                to="/fontList"
+                className={({ isActive }) => (isActive ? classes.active : undefined)}
+              >
+                폰트보기
+              </NavLink>
+            </div>
+            <div className={classes.smallBox}>
+              <NavLink
+                to="/faqPage"
+                className={({ isActive }) => (isActive ? classes.active : undefined)}
+              >
+                궁금해요
+              </NavLink>
+            </div>
+          </div>
+          <div className={classes.rightBox}>
+            <div className={classes.loginBox}>
+              <NavLink
+                to="/login"
+                className={({ isActive }) => (isActive ? classes.active : undefined)}
+              >
+                로그인
+              </NavLink>
+            </div>
+            {/* <NavLink
             to="/signup"
             className={({ isActive }) =>
               isActive ? classes.active : undefined
@@ -43,15 +55,18 @@ const NavBar: React.FC = () => {
           >
             Signup
           </NavLink> */}
-          <NavLink
-            to="/myPage"
-            className={({ isActive }) => (isActive ? classes.active : undefined)}
-          >
-            MyPage
-          </NavLink>
+            {/* <div className={classes.smallBox}>
+              <NavLink
+                to="/myPage"
+                className={({ isActive }) => (isActive ? classes.active : undefined)}
+              >
+                MyPage
+              </NavLink>
+            </div> */}
+          </div>
         </div>
       </nav>
     </>
-  )
-}
-export default NavBar
+  );
+};
+export default NavBar;
