11c2a7395e6d970fcf8d8442357b9a25d9808eef
김진주 <zizu4256@naver.com>
2023-10-24 12:17:05 +0900

    <feat>[#98-KJJ] 제작하기 페이지 3단계 이동 구현

--- frontend/src/pages/fontMakePage/FontMakePage.tsx
+++ frontend/src/pages/fontMakePage/FontMakePage.tsx
@@ -1,9 +1,25 @@
+import React, { useState } from "react"
+import classes from './FontMakePage.module.css';
+
 const FontMakePage: React.FC = () => {
+  const [step, setStep] = useState(1);
+
+  const handleNext = () => {
+    if (step < 3) {
+      setStep(prevStep => prevStep + 1);
+    }
+  }
+
   return (
     <>
-      <h1>Product</h1>
+    <div className={classes.stepContainer}>
+      <div className={step === 1 ? classes.nowStep : classes.restStep}><p>준비하기</p></div>
+      <div className={step === 2 ? classes.nowStep : classes.restStep}><p>손글씨 업로드</p></div>
+      <div className={step === 3 ? classes.nowStep : classes.restStep}><p>AI폰트 제작</p></div>
+      </div>
+      {step < 3 && <button onClick={handleNext}>다음</button>}
     </>
   )
 }
 
-export default FontMakePage
+export default FontMakePage;
