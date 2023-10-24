87c867c6365ddd2a7ebe5a6c3d0be0ac1f57e29a
김진주 <zizu4256@naver.com>
2023-10-24 17:07:10 +0900

    <feat>[#98-KJJ] 제작하기 페이지 준비물 및 유의사항 레이아웃 구성

--- frontend/src/pages/fontMakePage/FontMakePage.tsx
+++ frontend/src/pages/fontMakePage/FontMakePage.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react"
 import classes from './FontMakePage.module.css';
+import FontMakeStep1 from "componentPages/fontMakePageComponent/FontMakeStep1";
+import FontMakeStep2 from "componentPages/fontMakePageComponent/FontMakeStep2";
+import FontMakeStep3 from "componentPages/fontMakePageComponent/FontMakeStep3";
+
 
 const FontMakePage: React.FC = () => {
   const [step, setStep] = useState(1);
@@ -12,12 +16,19 @@ const FontMakePage: React.FC = () => {
 
   return (
     <>
-    <div className={classes.stepContainer}>
-      <div className={step === 1 ? classes.nowStep : classes.restStep}><p>준비하기</p></div>
-      <div className={step === 2 ? classes.nowStep : classes.restStep}><p>손글씨 업로드</p></div>
-      <div className={step === 3 ? classes.nowStep : classes.restStep}><p>AI폰트 제작</p></div>
+      <div className={classes.stepContainer}>
+        <div className={`${classes.stepBar} ${step === 1 ? classes.nowStep : classes.restStep}`}><p>준비하기</p></div>
+        <div className={`${classes.stepBar} ${step === 2 ? classes.nowStep : classes.restStep}`}><p>손글씨 업로드</p></div>
+        <div className={`${classes.stepBar} ${step === 3 ? classes.nowStep : classes.restStep}`}><p>AI폰트 제작</p></div>
       </div>
-      {step < 3 && <button onClick={handleNext}>다음</button>}
+      <div className={classes.contentContainer}>
+        {step === 1 && <FontMakeStep1 />}
+        {step === 2 && <FontMakeStep2 />}
+        {step === 3 && <FontMakeStep3 />}
+      </div>
+      <div className={classes.btnContainer}>
+        {step < 3 && <button onClick={handleNext} className={classes.nextBtn}>다음</button>}
+      </div>
     </>
   )
 }
