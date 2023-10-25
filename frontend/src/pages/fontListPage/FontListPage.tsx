cb9db503463594fb58c5f765ade61adc6e411a33
김진주 <zizu4256@naver.com>
2023-10-25 18:49:38 +0900

    <feat>[#101-KJJ] 폰트보기 페이지 필터링 드롭다운 구현

--- frontend/src/pages/fontListPage/FontListPage.tsx
+++ frontend/src/pages/fontListPage/FontListPage.tsx
@@ -1,14 +1,31 @@
-import React from "react"
+import React, { useState } from "react";
 import classes from "./FontListPage.module.css"
-import { FaSistrix } from 'react-icons/fa';
+import { FaSistrix, FaAngleDown } from 'react-icons/fa';
 import FontBoxComponent from "./fontListPageComponents/FontBoxComponent";
 
 const FontListPage: React.FC = () => {
+  const [showFilterOptions, setShowFilterOptions] = useState(false);
 
   const renderFontBoxes = () => {
+    console.log("Rendering filter options");
     return Array(8).fill(null).map((_, index) => <FontBoxComponent key={index} />);
   }
-  
+
+  const options = ["단정한", "가지런한", "둥글둥글", "네모네모", "삐뚤빼뚤", "귀여운", "문서체", "어른같은", "아이같은", "자유로운"];
+
+  const renderFilterOptions = () => {
+    return (
+      <div className={classes.filterOptions}>
+        {options.map((option, index) => (
+          <label key={index}>
+            <input type="checkbox" className={classes.checkbox} />
+            {" " + option}
+          </label>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <>
       <div className={classes.topContainer}>
@@ -24,8 +41,18 @@ const FontListPage: React.FC = () => {
           />
           <FaSistrix size={24} color="black" />
         </div>
-      <div>폰트 필터링</div>
+        <div className={classes.filterBarWrapper}>
+          <div
+            className={`${classes.filterBar} ${showFilterOptions ? classes.filterBarActive : ""}`}
+            onClick={() => setShowFilterOptions(!showFilterOptions)}
+          >
+            폰트 필터링
+            <FaAngleDown size={22} color="gray" style={{ marginLeft: '4px' }} className={`${classes.filterIcon} ${showFilterOptions ? classes.filterIconActive : ""}`} />
+          </div>
+          {showFilterOptions && renderFilterOptions()}
+        </div>
       </div>
+
       <div className={classes.fontBoxContainer}>
         {renderFontBoxes()}
       </div>
