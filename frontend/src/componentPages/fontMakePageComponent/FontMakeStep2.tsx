9d7c7094428110af54d6625f06edce2e142277e6
김진주 <zizu4256@naver.com>
2023-10-25 12:12:24 +0900

    <feat>[#101-KJJ] 제작하기 2단계 파일 업로드 및 삭제 구현

--- frontend/src/componentPages/fontMakePageComponent/FontMakeStep2.tsx
+++ frontend/src/componentPages/fontMakePageComponent/FontMakeStep2.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './FontMakeStep2.module.css'
 import MinuGuide from './fontMakePageAssets/guideline_ex.png'
 
 import { FaRegTimesCircle } from "react-icons/fa";
 
 const FontMakeStep2: React.FC = () => {
+  const [fileNames, setFileNames] = useState<string[]>([]);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      const newFileNames: string[] = [];
+      for (let i = 0; i < files.length; i++) {
+        newFileNames.push(files[i].name);
+      }
+      setFileNames(prev => [...prev, ...newFileNames]);
+    }
+  };
+
+  const removeFile = (index: number) => {
+    setFileNames(prev => prev.filter((_, i) => i !== index));
+  };
+
   return (
     <>
       <div className={classes.loadContainer}>
@@ -14,18 +31,28 @@ const FontMakeStep2: React.FC = () => {
             손글씨를 작성하고, 업로드 해주세요.
           </div>
           <img src={MinuGuide} alt="MinuGuide" className={classes.minuGuidImg} />
+          {/* 다운로드 기능 구현 필요 */}
           <button className={classes.downBtn}>다운로드</button>
         </div>
 
         <div className={classes.upLoadContainer}>
           <div className={classes.upLoadBar}>
-            파일 업로드
+            <input
+              type="file"
+              onChange={handleFileChange}
+              style={{ display: 'none' }}
+              id="fileInput"
+            />
+            <label htmlFor="fileInput">파일 업로드 click!</label>
           </div>
           <div className={classes.upLoadList}>
-            <div className={classes.upLoadFile}>
-              파일 목록
-              <FaRegTimesCircle style={{ marginLeft: '10px' }} />
-            </div>
+            {fileNames.map((name, index) => (
+              <div key={index} className={classes.upLoadFile}>
+                {name}
+                {/* 아이콘 클릭 시 파일명 삭제 */}
+                <FaRegTimesCircle className={classes.deleteIcon} onClick={() => removeFile(index)} />
+              </div>
+            ))}
           </div>
           <div className={classes.info}>
             ※ 또박또박 폰트 제작에는 약간의 시간이 소요되며, {'\n'}
