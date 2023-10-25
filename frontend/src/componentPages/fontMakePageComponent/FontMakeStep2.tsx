8341104a9a35f96593da4e4bd63738b88ad667a2
김진주 <zizu4256@naver.com>
2023-10-25 11:44:49 +0900

    <feat>[#101-KJJ] 제작하기 2단계 손글씨 업로드 레이아웃 구성

--- frontend/src/componentPages/fontMakePageComponent/FontMakeStep2.tsx
+++ frontend/src/componentPages/fontMakePageComponent/FontMakeStep2.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import classes from './FontMakeStep2.module.css'
-// import { FaRegTimesCircle } from "react-icons/fa";
+import MinuGuide from './fontMakePageAssets/guideline_ex.png'
+
+import { FaRegTimesCircle } from "react-icons/fa";
 
 const FontMakeStep2: React.FC = () => {
   return (
     <>
-      <h1>FontMakeStep2</h1>
+      <div className={classes.loadContainer}>
+        <div className={classes.downLoadContainer}>
+          <div className={classes.info}>
+            또박또박 손글씨 가이드라인을 다운받아 {'\n'}
+            손글씨를 작성하고, 업로드 해주세요.
+          </div>
+          <img src={MinuGuide} alt="MinuGuide" className={classes.minuGuidImg} />
+          <button className={classes.downBtn}>다운로드</button>
+        </div>
 
-      {/* 가이드라인 png 이미지 */}
-
-
-      <button className={classes.downBtn}>다운로드</button>
-
-
-      {/* 업로드된 파일 X 아이콘 */}
-      {/* <FaRegTimesCircle /> */}
-
+        <div className={classes.upLoadContainer}>
+          <div className={classes.upLoadBar}>
+            파일 업로드
+          </div>
+          <div className={classes.upLoadList}>
+            <div className={classes.upLoadFile}>
+              파일 목록
+              <FaRegTimesCircle style={{ marginLeft: '10px' }} />
+            </div>
+          </div>
+          <div className={classes.info}>
+            ※ 또박또박 폰트 제작에는 약간의 시간이 소요되며, {'\n'}
+            완료 시 메일을 발송해드립니다.
+          </div>
+        </div>
+      </div>
     </>
   )
 }
