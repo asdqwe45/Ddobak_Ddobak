66176995e36d78edc9c93377c06224dadcdf5dcb
김진주 <zizu4256@naver.com>
2023-10-25 09:44:56 +0900

    <feat>[#98-KJJ] 전역에 SUIT 폰트 적용

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/index';
+
+// font
+import './common/font/fonts.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
+  <div style={{ fontFamily: 'SUIT-Regular' }}>
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
+   </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
