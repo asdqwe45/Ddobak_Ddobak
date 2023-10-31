00975a9533dd2fa06c7d25294d379bd41e609155
이민규 <leeminkyu1212@gmail.com>
2023-10-31 12:14:18 +0900

    <feat>#132-LMK 이미지 정렬 및 자르기 API 구현 ai-server/FastApiProjet AI 서버 이미지 정렬 및 자르기 구현 하였습니다.

--- ai-server/FastApiProjet/image_align/controller/controller.py
+++ ai-server/FastApiProjet/image_align/controller/controller.py
@@ -4,10 +4,11 @@ from fastapi.responses import StreamingResponse
 
 router = APIRouter()
 
-@router.post("/process-image/")
+
+@router.post("")
 async def process_image_endpoint(file: UploadFile = File(...)):
     file_bytes = await file.read()  # 파일을 바이트로 읽는다.
-    processed_image =process_image(file_bytes)
+    processed_image = process_image(file_bytes)
 
     return StreamingResponse(processed_image, media_type="image/png",
                              headers={'Content-Disposition': 'attachment; filename="image.png"'})
