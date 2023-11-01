c7842d40f235efee4efe94df8e0d29e8c7318bfd
이재명 <iamwoaud1004@naver.com>
2023-11-01 17:06:56 +0900

    <fix>[#118-LJM] 폰트 제작 기능 구현

--- backend/font-service/src/main/java/com/fontservice/controller/FontController.java
+++ backend/font-service/src/main/java/com/fontservice/controller/FontController.java
@@ -1,24 +1,19 @@
 package com.fontservice.controller;
 
+import com.fontservice.domain.Font;
+import com.fontservice.repository.FontQueryRepository;
 import com.fontservice.service.FontImageService;
 import com.fontservice.service.FontService;
-import com.fontservice.service.S3Service;
 import lombok.RequiredArgsConstructor;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.client.RestTemplate;
 import java.io.File;
 import java.io.IOException;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -37,6 +32,7 @@ public class FontController {
     ){}
 
 
+
     @Autowired
     private final FontImageService fontImageService;
     @Autowired
@@ -44,13 +40,16 @@ public class FontController {
 
 
     @PostMapping("/test")
-    public String test(@RequestPart(value = "data") FontWebRequest req){
+    public String test(){
         return "test";
     }
-
+//    @GetMapping("/list")
+//    public List<FontQueryRepository.FontListWebResponse> getFontAll(){
+//        return fontService.getFontAll();
+//    }
 
     @PostMapping("/sort")
-    public ResponseEntity<String> sort(@RequestParam("file") MultipartFile[] files, @RequestParam Long producer_id){
+    public ResponseEntity<String> sort(@RequestParam("file") MultipartFile[] files){
         System.out.println("0");
         try {
             String s3Url = new String();
@@ -60,16 +59,16 @@ public class FontController {
                 if (tempOutputFile.length() == 0) {
                     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("파일 형식이 올바르지 않습니다.");
                 }
-
+                System.out.println("2");
                 // AI에 넘겨주고 받기
-                s3Url = s3Url + fontImageService.getS3Url(tempOutputFile);
+                s3Url = s3Url + fontImageService.getS3SortUrl(tempOutputFile);
                 s3Url = s3Url + "$";
             }
             if(s3Url == null){
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("AI response의 파일 타입이 올바르지 않습니다.");
             }
 
-            fontService.addFont(producer_id, s3Url);
+
             return ResponseEntity.ok(String.join(", ", s3Url));  // 모든 S3 URL을 반환합니다.
         } catch (IOException e) {
             e.printStackTrace();
@@ -79,22 +78,35 @@ public class FontController {
 
 
     @PostMapping("/make")
-    public ResponseEntity<String> makeFile(@RequestPart("file") MultipartFile file, @RequestPart(value = "data") FontWebRequest req) {
+    public ResponseEntity<String> makeFile(@RequestPart("file") MultipartFile[] files, @RequestParam("producer_id") Long producer_id, @RequestParam("font_sort_url") String font_sort_url) {
         try {
             System.out.println("1");
-            File tempFile = File.createTempFile("source",".png");
+            File tempFile1 = File.createTempFile("source",".png");
+            File tempFile2 = File.createTempFile("source",".png");
+
             System.out.println("2");
 
-            file.transferTo(tempFile);
+            files[0].transferTo(tempFile1);
             System.out.println("3");
 
-            String s3Url = fontImageService.getS3Url(tempFile);
+            files[1].transferTo(tempFile2);
+
+            List<File> tempFile = new ArrayList<>();
+            System.out.println("3");
+
+            tempFile.add(tempFile1);
+            System.out.println("3");
+
+            tempFile.add(tempFile2);
+            System.out.println("3");
+
+            String s3Url = fontImageService.getS3MakeUrl(tempFile);
             System.out.println("4");
 
             if(s3Url.length()==0){
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("AI response의 파일 타입이 올바르지 않습니다.");
             }
-            //fontService.makeFont(s3Url,req);
+            fontService.addFont(producer_id,font_sort_url, s3Url);
 
             return ResponseEntity.ok(s3Url);
         } catch (IOException e) {
