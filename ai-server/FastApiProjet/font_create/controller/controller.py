ef1cbb4f0351df27b451bed576fa53a41112114b
이민규 <leeminkyu1212@gmail.com>
2023-11-02 17:59:15 +0900

    <feat>#132-LMK 배포 준비중 ai-server/FastApiProjet 배포 준비를 위한 API 테스트 중입니다.

--- ai-server/FastApiProjet/font_create/controller/controller.py
+++ ai-server/FastApiProjet/font_create/controller/controller.py
@@ -3,6 +3,7 @@ from fastapi.responses import StreamingResponse
 from ..service.download_image_from_url import download_image_from_url
 from ..service.korean_crop import crop_image_uniform
 from ..service.english_crop import crop_image_uniform_eng
+
 router = APIRouter()
 
 ########
@@ -11,24 +12,24 @@ from io import BytesIO
 from fastapi import File, UploadFile
 
 
-@router.post("/eng")
-async def crop_template_kor(file: UploadFile = File(...)):
-  image_bytes = await file.read()
+@router.post("/image_crop")
+async def crop_template(kor_file: UploadFile = File(...), eng_file: UploadFile = File(...)):
+    kor_image_bytes = await kor_file.read()
+    eng_image_bytes = await eng_file.read()
+    kor_img = Image.open(BytesIO(kor_image_bytes)).convert('L')
+    message1 = crop_image_uniform(kor_img)
+    eng_img = Image.open(BytesIO(eng_image_bytes)).convert('L')
+    message2 = crop_image_uniform_eng(eng_img)
 
-  img = Image.open(BytesIO(image_bytes)).convert('L')
-  message = crop_image_uniform(img)
-  return message
+    if (message1 == message2): return message1
 
-@router.post("/eng")
-async def crop_template_eng(file: UploadFile = File(...)):
-  image_bytes = await file.read()
+@router.post("/sample_font")
+async def crop_template(kor_file: UploadFile = File(...), eng_file: UploadFile = File(...)):
+    kor_image_bytes = await kor_file.read()
+    eng_image_bytes = await eng_file.read()
+    kor_img = Image.open(BytesIO(kor_image_bytes)).convert('L')
+    message1 = crop_image_uniform(kor_img)
+    eng_img = Image.open(BytesIO(eng_image_bytes)).convert('L')
+    message2 = crop_image_uniform_eng(eng_img)
+    
 
-  img = Image.open(BytesIO(image_bytes)).convert('L')
-  message = crop_image_uniform_eng(img)
-  return message
-
-# async def crop_template_kor(image_url: str = Query(..., alias="image-url")):
-#   image = download_image_from_url(image_url)
-#   message = crop_image_uniform(image)
-#
-#   return message
