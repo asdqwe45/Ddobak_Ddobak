95583c5120bae3f4585cfeab29a96317403d9a96
김진주 <zizu4256@naver.com>
2023-11-02 16:23:33 +0900

    <feat>[#137-KJJ] 제작하기 페이지 4단계로 수정 및 폰트 정보 입력 결제 버튼 추가

--- frontend/src/pages/fontMakePage/FontMakePage.tsx
+++ frontend/src/pages/fontMakePage/FontMakePage.tsx
@@ -3,12 +3,13 @@ import classes from './FontMakePage.module.css';
 import FontMakeStep1 from 'componentPages/fontMakePageComponent/FontMakeStep1';
 import FontMakeStep2 from 'componentPages/fontMakePageComponent/FontMakeStep2';
 import FontMakeStep3 from 'componentPages/fontMakePageComponent/FontMakeStep3';
+import FontOptionPage from 'componentPages/fontMakePageComponent/FontOptionPage';
 
 const FontMakePage: React.FC = () => {
   const [step, setStep] = useState(1);
 
   const handleNext = () => {
-    if (step < 3) {
+    if (step < 4) {
       setStep((prevStep) => prevStep + 1);
     }
   };
@@ -23,13 +24,17 @@ const FontMakePage: React.FC = () => {
           <p>손글씨 업로드</p>
         </div>
         <div className={`${classes.stepBar} ${step === 3 ? classes.nowStep : classes.restStep}`}>
+          <p>폰트 정보 입력</p>
+        </div>
+        <div className={`${classes.stepBar} ${step === 4 ? classes.nowStep : classes.restStep}`}>
           <p>AI폰트 제작</p>
         </div>
       </div>
       <div className={classes.contentContainer}>
         {step === 1 && <FontMakeStep1 />}
         {step === 2 && <FontMakeStep2 />}
-        {step === 3 && <FontMakeStep3 />}
+        {step === 3 && <FontOptionPage />}
+        {step === 4 && <FontMakeStep3 />}
       </div>
       <div className={classes.btnContainer}>
         {step < 3 && (
