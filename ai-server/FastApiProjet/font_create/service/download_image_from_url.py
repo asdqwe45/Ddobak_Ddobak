45b07e623155638b817564d61c8d856dd24fcf24
이민규 <leeminkyu1212@gmail.com>
2023-11-03 13:15:47 +0900

    Revert "Merge branch 'feat/132-LMK' into 'develop'"
    This reverts merge request !91

--- ai-server/FastApiProjet/font_create/service/download_image_from_url.py
+++
@@ -1,7 +0,0 @@
-import requests
-from io import BytesIO
-from PIL import Image
-def download_image_from_url(url):
-    response = requests.get(url)
-    img = Image.open(BytesIO(response.content)).convert('L')
-    return img
