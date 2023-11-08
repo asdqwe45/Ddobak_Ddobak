aa236cec1cebf43d64c0e80fb97df7846ef18a4e
vdomobv <vdomobv@naver.com>
2023-11-09 00:59:25 +0900

    <feat>[#187-LJE] 찜 목록 가져오기 API 수정

--- backend/Ddobak/src/main/java/com/ddobak/favorite/controller/FavoriteController.java
+++ backend/Ddobak/src/main/java/com/ddobak/favorite/controller/FavoriteController.java
@@ -2,8 +2,8 @@ package com.ddobak.favorite.controller;
 
 import com.ddobak.favorite.entity.Favorite;
 import com.ddobak.favorite.service.FavoriteService;
+import com.ddobak.font.dto.response.DibbedFontInfo;
 import com.ddobak.font.entity.Font;
-import com.ddobak.font.service.FontService;
 import com.ddobak.security.util.LoginInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,8 +12,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -21,7 +19,6 @@ import java.util.List;
 @RequestMapping("/api/v1/favorite") // changed to plural form to be more RESTful
 public class FavoriteController {
 
-    private final FontService fontService;
     private final FavoriteService favoriteService;
 
     // 찜 확인
@@ -57,21 +54,40 @@ public class FavoriteController {
 
     // 찜 목록 가져 오기
     @GetMapping("/list")
-    public ResponseEntity<List<Font>> getDibsByMember(@AuthenticationPrincipal LoginInfo loginInfo) {
+    public ResponseEntity<List<DibbedFontInfo>> getDibsByMember(@AuthenticationPrincipal LoginInfo loginInfo) {
         Long memberId = loginInfo.id();
         List<Favorite> favorites = favoriteService.findByMemberId(memberId);
 
         if (favorites.isEmpty()) {
             return ResponseEntity.noContent().build();
         } else {
-            ArrayList<Font> result = new ArrayList<>();
+            List<DibbedFontInfo> result = new ArrayList<>();
             for (Favorite favorite : favorites) {
-                Long fontId = favorite.getFont().getId(); // 즐겨찾기된 폰트의 ID를 가져오는 메소드
-                result.add(fontService.findByFontId(fontId));
+                Font font = favorite.getFont(); // 즐겨찾기된 폰트 엔티티를 가져오는 메소드
+                if (font != null) {
+                    DibbedFontInfo dibbedFontInfo = getDibbedFontInfo(font);
+                    result.add(dibbedFontInfo);
+                }
             }
             return ResponseEntity.ok(result);
-
         }
     }
+
+    private static DibbedFontInfo getDibbedFontInfo(Font font) {
+        Long fontId = font.getId(); // 폰트 ID
+        String producerName = font.getProducer().getUsername(); // 제조사 이름
+        String fontFileUrl = font.getFont_file_url(); // 폰트 파일 URL
+        String fontName = font.getKor_font_name(); // 폰트 이름
+
+        // 여기서 dibCheck는 즐겨찾기 여부를 나타내는데, 이미 즐겨찾기 목록에서 가져오고 있으므로 항상 true입니다.
+        return new DibbedFontInfo(
+                fontId,
+                true, // 즐겨찾기된 아이템으로 가정
+                producerName,
+                fontFileUrl,
+                fontName
+        );
+    }
+
 
 }
