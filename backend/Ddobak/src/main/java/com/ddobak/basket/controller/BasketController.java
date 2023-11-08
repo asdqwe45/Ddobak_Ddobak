967cd108d991e2571852805f0d9b74d379d57637
이재명 <iamwoaud1004@naver.com>
2023-11-09 00:18:42 +0900

    <feat>[#183-LJM] 장바구니 추가 기능 구현

--- backend/Ddobak/src/main/java/com/ddobak/basket/controller/BasketController.java
+++ backend/Ddobak/src/main/java/com/ddobak/basket/controller/BasketController.java
@@ -1,22 +1,35 @@
-//package com.ddobak.basket.controller;
-//
-//import com.ddobak.security.util.LoginInfo;
-//import lombok.NoArgsConstructor;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/api/v1/basket")
-//@NoArgsConstructor
-//@RequiredArgsConstructor
-//public class BasketController {
-//
-////    @GetMapping("/list")
-////    public ResponseEntity<?> getBasketList(@AuthenticationPrincipal LoginInfo loginInfo){
-////
-////    }
-//}
+package com.ddobak.basket.controller;
+
+import com.ddobak.basket.service.BasketService;
+import com.ddobak.security.util.LoginInfo;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/basket")
+@RequiredArgsConstructor
+@Slf4j
+public class BasketController {
+
+    private final BasketService basketService;
+
+    @GetMapping("/list")
+    public ResponseEntity<?> getBasketList(@AuthenticationPrincipal LoginInfo loginInfo){
+
+
+
+        return ResponseEntity.ok(null);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addBasket(@RequestParam Long fontId, @AuthenticationPrincipal LoginInfo loginInfo){
+        System.out.println("??");
+        basketService.addBasket(fontId,loginInfo);
+
+        return ResponseEntity.ok("success");
+    }
+}
