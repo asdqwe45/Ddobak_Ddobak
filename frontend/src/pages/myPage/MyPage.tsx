7136dd8bd43b497f6bc88c86373736435d818ddd
vdomobv <vdomobv@naver.com>
2023-11-09 14:10:46 +0900

    <feat>[#184-LJE] 찜 삭제 연결

--- frontend/src/pages/myPage/MyPage.tsx
+++ frontend/src/pages/myPage/MyPage.tsx
@@ -226,7 +226,13 @@ const MyPage: React.FC = () => {
   
   const clickBookmarkButton = (dib : DibType) => {
     console.log("click");
-    dibRemoveAPI(dib.fontId);
+    dibRemoveAPI(dib.fontId).then(() =>{
+      dibListAPI().then((response) => {
+        console.log(response);
+        setDibList(response);
+      });
+    })
+    
   }
 
   // 닉네임 수정하기 마우스 호버시
