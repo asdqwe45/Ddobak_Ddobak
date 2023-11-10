ed8abe251487f44839aa4b4d3fba46150bd6e362
이재명 <iamwoaud1004@naver.com>
2023-11-10 23:12:40 +0900

    <feat>[#183-LJM] 장바구니 폰트 선택 삭제 기능 구현

--- backend/Ddobak/src/main/java/com/ddobak/cart/repository/CartRepository.java
+++ backend/Ddobak/src/main/java/com/ddobak/cart/repository/CartRepository.java
@@ -2,8 +2,12 @@ package com.ddobak.cart.repository;
 
 
 import com.ddobak.cart.entity.Cart;
+import com.ddobak.font.entity.Font;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +16,9 @@ public interface CartRepository extends JpaRepository<Cart,Long> {
     Boolean existsByMemberIdAndFontId(Long memberId,Long fontId);
 
     List<Cart> findAllByMemberIdOrderByCreatedAtDesc(Long memberId);
+
+    @Transactional
+    @Modifying
+    @Query("delete from Cart c where c.font.id in :fontList and c.member.id = :memberId")
+    void deleteFontsFromCart(@Param("memberId") Long memberId, @Param("fontList") List<Long> fontList);
 }
