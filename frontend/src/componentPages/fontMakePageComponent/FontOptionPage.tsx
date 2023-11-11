add8dd0bb74c1907bb9bd6c97e370c3df193410c
김진주 <zizu4256@naver.com>
2023-11-11 21:35:32 +0900

    <feat>[#198-KJJ] 제작하기 페이지-폰트 이름 설정 중복확인 완료

--- frontend/src/componentPages/fontMakePageComponent/FontOptionPage.tsx
+++ frontend/src/componentPages/fontMakePageComponent/FontOptionPage.tsx
@@ -9,6 +9,8 @@ import TermsAgreement from 'common/checkButton/TermsAgreement';
 import { useDispatch } from 'react-redux';
 import { pointPayModalActions } from 'store/pointPayModalSlice';
 
+import { axiosWithAuth } from 'https/http';
+
 const FontOptionPage: React.FC = () => {
   const [inputValue, setInputValue] = useState('');
 
@@ -34,36 +36,96 @@ const FontOptionPage: React.FC = () => {
     dispatch(pointPayModalActions.toggle());
   };
 
+  const [korFontName, setKorFontName] = useState('');
+
+  // 폰트명 입력 핸들러 함수
+  const handleKorNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKorFontName(event.target.value);
+  };
+
+  const korNameCheck = async () => {
+    if (!korFontName.trim()) {
+      alert('폰트 이름을 입력해 주세요!');
+      return;
+    }
+    try {
+      const params = { korFontName: korFontName}
+      const response = await axiosWithAuth.get('/font/name/check', { params });
+      if (!response.data) { // false: 사용가능 | true: 중복
+        alert('사용가능한 이름입니다.');
+      } else {
+        alert('중복된 폰트 이름입니다.');
+        setKorFontName('')
+      }
+    } catch (error) {
+      console.error('한글명 중복체크 오류 발생:', error);
+    }
+  };
+
+  const [engFontName, setEngFontName] = useState('');
+
+  // 파일명(영문) 입력 핸들러 함수
+  const handleEngNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    const regex = /^[A-Za-z0-9]*$/;
+  
+    if (value === '' || regex.test(value)) {
+      setEngFontName(value);
+    } else {
+      alert('영문과 숫자만 입력할 수 있습니다.');
+    }
+  };
+
+  const engNameCheck = async () => {
+    if (!engFontName.trim()) {
+      alert('폰트 이름을 입력해 주세요!');
+      return;
+    }
+    try {
+      const params = { engFontName: engFontName}
+      const response = await axiosWithAuth.get('/font/name/check', { params });
+      if (!response.data) { // false: 사용가능 | true: 중복
+        alert('사용가능한 파일명입니다.');
+      } else {
+        alert('중복된 파일명입니다.');
+        setEngFontName('')
+      }
+    } catch (error) {
+      console.error('파일명(영문) 중복체크 오류 발생:', error);
+    }
+  };
+
   return (
     <>
       <div className={classes.container}>
-        {/* <div style={{ marginTop: '60px' }}>
-        <PageTitle>
-          <span>폰트 정보 입력</span>
-        </PageTitle>
-      </div> */}
         <div className={classes.fontNameContainer}>
           <BoxTitle>폰트 이름 설정</BoxTitle>
           <div className={classes.name}>
+
             <div className={classes.nameInput} style={{ flexGrow: '1' }}>
-              <InputTitle>한글명</InputTitle>
+              <InputTitle>폰트명</InputTitle>
               <input
                 type="text"
                 placeholder="예시) 또박또박_글씨체"
+                value={korFontName}
+                onChange={handleKorNameChange}
                 style={{ width: '20vw', height: '50px' }}
-              />
-
-              <button>중복확인</button>
+                />
+              <button onClick={korNameCheck}>중복확인</button>
             </div>
+
             <div className={classes.nameInput} style={{ flexGrow: '2' }}>
-              <InputTitle>영문명</InputTitle>
+              <InputTitle>파일명(영문)</InputTitle>
               <input
                 type="text"
                 placeholder="예시) ddobak_test"
+                value={engFontName}
+                onChange={handleEngNameChange}
                 style={{ width: '20vw', height: '50px' }}
               />
-              <button>중복확인</button>
+              <button onClick={engNameCheck}>중복확인</button>
             </div>
+
           </div>
         </div>
         <br />
@@ -82,80 +144,27 @@ const FontOptionPage: React.FC = () => {
             />
           </div>
         </div>
-        <br />
         <hr />
         <br />
 
-        {/* 저작권 관계 명시 */}
-        <div className={classes.relationContainer}>
-          <BoxTitle>저작권 관계 명시</BoxTitle>
-          <div>
-            <div className={classes.rowContainer}>
-              <InputTitle>제작 주문자와 저작권자의 관계</InputTitle>
-              <RadioBtn
-                options={['동일인', '동일인 아님']}
-                name="same"
-                // onChange={setSelectedOption}
-                style={{ marginLeft: '40px' }}
-              />
-            </div>
-          </div>
-          {/* 동일인 선택시 자동으로 입력되도록 구현 */}
-          <div className={classes.rowContainer}>
-            <InputTitle>저작권자 이름</InputTitle>
-            <input type="text" style={{ width: '15vw', height: '50px' }} />
-            <div style={{ marginLeft: '40px' }}>
-              ※ 작성된 이름으로 폰트의 저작권자 명이 표시됩니다.
-            </div>
-          </div>
-        </div>
-        <br />
-        <hr />
-        <br />
-
-        {/* 허용 여부 라디오버튼 */}
+        {/* 상태 설정 라디오버튼 */}
         <div className={classes.radioBtnContainer}>
-          <BoxTitle>허용 여부 설정</BoxTitle>
+          <BoxTitle>상태 설정</BoxTitle>
           <div className={classes.rowContainer}>
             <InputTitle style={{ width: '20vw' }}>공개 여부</InputTitle>
             <RadioBtn
               options={['공개', '비공개']}
               name="open"
-              // onChange={setSelectedOption}
+            // onChange={setSelectedOption}
             />
           </div>
-          <div className={classes.rowContainer}>
-            <InputTitle style={{ width: '20vw' }}>상업적 이용 허용</InputTitle>
-            <RadioBtn
-              options={['허용', '비허용']}
-              name="use"
-              // onChange={setSelectedOption}
-            />
-          </div>
-          <div className={classes.rowContainer}>
-            <InputTitle style={{ width: '20vw' }}>저작자 표시</InputTitle>
-            <RadioBtn
-              options={['필수', '선택']}
-              name="person"
-              // onChange={setSelectedOption}
-            />
-          </div>
-          <div className={classes.rowContainer}>
-            <InputTitle style={{ width: '20vw' }}>폰트 변형</InputTitle>
-            <RadioBtn
-              options={['가능', '금지']}
-              name="edit"
-              // onChange={setSelectedOption}
-            />
-          </div>
-          <br />
-          <hr />
+
           <div className={classes.rowContainer}>
             <InputTitle style={{ width: '20vw' }}>판매 금액</InputTitle>
             <RadioBtn
               options={['유료', '무료']}
               name="charge"
-              // onChange={setSelectedOption}
+            // onChange={setSelectedOption}
             />
             <InputTitle style={{ marginLeft: '100px' }}>판매 금액 설정</InputTitle>
             <input type="text" style={{ width: '15vw', height: '50px' }} />
