118349d53b863a0ce8e2df2ecb399c9ba38ba7fb
이재명 <iamwoaud1004@naver.com>
2023-11-12 18:45:48 +0900

    <fix>[#183-LJM] 폰트 목록 response 수정

--- backend/Ddobak/src/main/java/com/ddobak/font/dto/response/FontResponse.java
+++ backend/Ddobak/src/main/java/com/ddobak/font/dto/response/FontResponse.java
@@ -11,12 +11,14 @@ public class FontResponse {
     private String font_file_url;
     private Boolean dibCheck;
     private Long producer_id;
-    public FontResponse(Long id, String kor_font_name, String producer_name, String font_file_url, Boolean dibCheck, Long producer_id) {
+    private int price;
+    public FontResponse(Long id, String kor_font_name, String producer_name, String font_file_url, Boolean dibCheck, Long producer_id, int price) {
         this.font_id = id;
         this.kor_font_name = kor_font_name;
         this.producer_name = producer_name;
         this.font_file_url = font_file_url;
         this.dibCheck = dibCheck;
         this.producer_id=producer_id;
+        this.price = price;
     }
 }
