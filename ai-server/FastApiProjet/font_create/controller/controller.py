732a2c2764abaecd3d17ed546297261da3f87adb
이민규 <leeminkyu1212@gmail.com>
2023-11-13 13:14:53 +0900

    <feat>#188-LMK AI 서버 설정중 /home/sosohang/S09P31C208/ai-server/FastApiProjet
    ai 서버 api 개발중입니다

--- ai-server/FastApiProjet/font_create/controller/controller.py
+++ ai-server/FastApiProjet/font_create/controller/controller.py
@@ -1,15 +1,16 @@
+from fastapi import File, UploadFile, Form
+from io import BytesIO
+from PIL import Image
+from ..service.compress import create_zip_from_folder
 from fastapi import APIRouter, Query
 from fastapi.responses import FileResponse
+
 from ..service.template_crop import crop_image_uniform
 from ..service.font_image_create import font_image_create
 import os
 
 router = APIRouter()
-from ..service.compress import create_zip_from_folder
 ########
-from PIL import Image
-from io import BytesIO
-from fastapi import File, UploadFile,Form
 
 
 @router.post("/image_crop")
@@ -28,14 +29,17 @@ async def crop_template(kor_file: UploadFile = File(...), eng_file: UploadFile =
     eng_image_bytes = await eng_file.read()
     kor_img = Image.open(BytesIO(kor_image_bytes)).convert('L')
     unique_kor_dir = crop_image_uniform(kor_img, './cropped_output_kor')
+
     eng_img = Image.open(BytesIO(eng_image_bytes)).convert('L')
     unique_eng_dir = crop_image_uniform(eng_img, './cropped_output_eng')
 
-    response = font_image_create(unique_kor_dir,unique_eng_dir, sample=True)
+    response = font_image_create(unique_kor_dir, unique_eng_dir, sample=True)
 
     return response
+
+
 @router.post("/create_font")
-async def create_font(kor_file:UploadFile=File(...),eng_file:UploadFile=File(...),font_name:str=Form(None)):
+async def create_font(kor_file: UploadFile = File(...), eng_file: UploadFile = File(...), font_name: str = Form(None)):
     kor_image_bytes = await kor_file.read()
     eng_image_bytes = await eng_file.read()
     kor_img = Image.open(BytesIO(kor_image_bytes)).convert('L')
@@ -43,6 +47,7 @@ async def create_font(kor_file:UploadFile=File(...),eng_file:UploadFile=File(...
     eng_img = Image.open(BytesIO(eng_image_bytes)).convert('L')
     unique_eng_dir = crop_image_uniform(eng_img, './cropped_output_eng')
 
-    response = font_image_create(unique_kor_dir,unique_eng_dir, sample=False,font_name=font_name)
+    response = font_image_create(
+        unique_kor_dir, unique_eng_dir, sample=False, font_name=font_name)
 
     return response
--- ai-server/FastApiProjet/font_create/service/template_crop.py
+++ ai-server/FastApiProjet/font_create/service/template_crop.py
@@ -6,12 +6,14 @@ import numpy as np
 from datetime import datetime
 from pathlib import Path
 import os
+from .font_image_align import center_letter_in_image_128x128,process_folder
+from .handwriting_infer import image_preprocess
 
 
 def crop_image_uniform(template_image, base_output_dir):
     # //임시 폴더 생성
     unique_dir_name = datetime.now().strftime('%Y%m%d_%H%M%S')
-    output_dir = Path(base_output_dir) / unique_dir_name/ unique_dir_name
+    output_dir = Path(base_output_dir) / unique_dir_name / unique_dir_name
 
     # 해당 경로에 디렉토리가 존재하지 않는다면 생성
     os.makedirs(output_dir, exist_ok=True)
@@ -25,20 +27,23 @@ def crop_image_uniform(template_image, base_output_dir):
     #
     base_list = []
     if (base_output_dir == './cropped_output_kor'):
-        base_list = ["가", "깩", "낚", "댻", "떤", "렍", "멶", "볟", "뽈", "솱", "쐚", "욃",
-                     "죬", "쭕", "춾", "퀧", "튐", "퓹", "흢", "긧", "낐", "낭", "댖", "땿",
-                     "럨", "멑", "벺", "뼣"]
+        # base_list = ["가", "깩", "낚", "댻", "떤", "렍", "멶", "볟", "뽈", "솱", "쐚", "욃",
+        #              "죬", "쭕", "춾", "퀧", "튐", "퓹", "흢", "긧", "낐", "낭", "댖", "땿",
+        #              "럨", "멑", "벺", "뼣"]
+        base_list = ["값", "같", "곬", "곶", "깎", "넋", "늪", "닫", "닭", "닻", "됩", "뗌", "략", "몃", "밟", "볘",
+                     "뺐", "뽈", "솩", "쐐", "앉", "않", "얘", "얾", "엌", "옳", "읊", "죡", "쮜", "춰", "츄", "퀭",
+                     "틔", "핀", "핥", "훟"]
         rows = 3
         cols = 12
-        header_ratio = 16.5 / (16.5 + 42)
-        length = 28
+        header_ratio = 0.48 / (0.48 + 2.25)
+        length = 36
         img = template_image
         width, height = img.size
         cell_width = width / float(cols)
         cell_height = height / float(rows)
         header_offset = height / float(rows) * header_ratio
-        width_margin = cell_width * 0.02
-        height_margin = cell_height * 0.02
+        width_margin = cell_width * 0.07
+        height_margin = cell_height * 0.07
     elif (base_output_dir == './cropped_output_eng'):
         base_list = [
             "0041", "0042", "0043", "0044", "0045", "0046", "0047", "0048", "0049",
@@ -64,13 +69,10 @@ def crop_image_uniform(template_image, base_output_dir):
         cell_width = width / float(cols)
         cell_height = height / float(rows)
         header_offset = height / float(rows) * header_ratio
-        width_margin = cell_width * 0.05
-        height_margin = cell_height * 0.05
+        width_margin = cell_width * 0.1
+        height_margin = cell_height * 0.1
 
     # img = Image.open(template_image).convert('L')
-
-
-
 
     for j in range(0, rows):
         for i in range(0, cols):
@@ -99,15 +101,17 @@ def crop_image_uniform(template_image, base_output_dir):
                 break
             # code = hex(ord(korean_list[j * cols + i]))[2:]
             code = base_list[j * cols + i]
-            name = str(output_dir)+"/"+ code + ".png"
+            name = str(output_dir)+"/" + code + ".png"
             cropped_image = img.crop((left, upper, right, lower))
             cropped_image = cropped_image.resize((128, 128), Image.LANCZOS)
             # Increase constrast
             enhancer = ImageEnhance.Contrast(cropped_image)
             cropped_image = enhancer.enhance(1.5)
-            opencv_image = np.array(cropped_image)
-            opencv_image = bilateralFilter(opencv_image, 9, 30, 30)
-            cropped_image = Image.fromarray(opencv_image)
+            # opencv_image = np.array(cropped_image)
+            # opencv_image = bilateralFilter(opencv_image, 9, 30, 30)
+            # cropped_image = Image.fromarray(opencv_image)
+            cropped_image = center_letter_in_image_128x128(cropped_image)
             cropped_image.save(name)
-
+    image_preprocess(base_output_dir,unique_dir_name)
+    process_folder(f'{base_output_dir}/f{unique_dir_name}/f{unique_dir_name}')
     return unique_dir_name
