4adbf7920d71fa6208a2ce8cfb261f1781964f03
이규민 <lkm454545@naver.com>
2023-11-13 17:28:54 +0900

    <feat>[238-LKM]Emai] Email 템플릿 작성

--- backend/Ddobak/src/main/java/com/ddobak/member/service/EmailService.java
+++ backend/Ddobak/src/main/java/com/ddobak/member/service/EmailService.java
@@ -2,12 +2,17 @@ package com.ddobak.member.service;
 
 import com.ddobak.global.exception.ErrorCode;
 import com.ddobak.member.exception.EmailException;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.naming.Context;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.TemplateEngine;
 
 @Slf4j
 @Service
@@ -17,16 +22,6 @@ public class EmailService {
 
     private final JavaMailSender javaMailSender;
 
-    public void sendEmail(String toEmail, String title, String text) {
-        SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
-
-        try {
-            javaMailSender.send(emailForm);
-        } catch (RuntimeException e) {
-            throw new EmailException(ErrorCode.EMAIL_SEND_ERROR);
-        }
-    }
-
     private SimpleMailMessage createEmailForm(String toEmail, String title, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(toEmail);
@@ -34,5 +29,21 @@ public class EmailService {
         message.setText(text);
 
         return message;
+    }
+
+    public void sendHTMLEmail(String toEmail, String title, String htmlText) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+
+        try {
+            helper.setTo(toEmail);
+            helper.setSubject(title);
+            helper.setText(htmlText, true); // true는 HTML 형식으로 설정
+
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            log.error("Failed to send HTML email", e);
+            throw new EmailException(ErrorCode.EMAIL_SEND_ERROR);
+        }
     }
 }
