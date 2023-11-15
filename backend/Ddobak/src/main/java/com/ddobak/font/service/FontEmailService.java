ddf851ac5ccfad5a6832100f73aeec82f09990e9
이규민 <lkm454545@naver.com>
2023-11-15 15:25:39 +0900

    <feat>[238-LKM] 제작 완료 이메일 안내

--- backend/Ddobak/src/main/java/com/ddobak/font/service/FontEmailService.java
+++ backend/Ddobak/src/main/java/com/ddobak/font/service/FontEmailService.java
@@ -1,44 +1,40 @@
-package com.ddobak.member.service;
+package com.ddobak.font.service;
 
 import com.ddobak.global.exception.ErrorCode;
+import com.ddobak.member.entity.Member;
 import com.ddobak.member.exception.EmailException;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import org.thymeleaf.context.Context;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Slf4j
-@Service
-@Transactional
 @RequiredArgsConstructor
-public class EmailService {
+@Service
+public class FontEmailService {
 
     private final JavaMailSender javaMailSender;
     private final TemplateEngine templateEngine;
 
-
-    public String makeEmailTemplate(String authCode) {
+    private String makeEmailTemplate() {
         Context context = new Context();
-        context.setVariable("authCode", authCode);
+        return templateEngine.process("fontCreatedEmailTemplate", context);
 
-        return templateEngine.process("authenticationEmailTemplate", context);
     }
 
-    public void sendHTMLEmail(String toEmail, String title, String authCode) {
+    public void sendCompleteEmail(Member member ) {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
-        String htmlText = makeEmailTemplate(authCode);
+        String htmlText = makeEmailTemplate();
 
         try {
-            helper.setTo(toEmail);
-            helper.setSubject(title);
+            helper.setTo(member.getEmail());
+            helper.setSubject("또박 또박 폰트 완료 메일입니다.");
             helper.setText(htmlText, true); // true는 HTML 형식으로 설정
 
             javaMailSender.send(mimeMessage);
